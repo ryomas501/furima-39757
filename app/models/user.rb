44321bde7e926693b8c44d2d5e82de7450a3ed5a
ryomas501 <141004549+ryomas501@users.noreply.github.com>
2023-08-28 17:49:52 +0900

    userモデルのバリテーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :family_name, presence: true
-         validates :first_name, presence: true
-         validates :family_name_kana, presence: true
-         validates :first_name_kana, presence: true
          validates :birthday, presence: true
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+          validates :family_name
+          validates :first_name
+        end
+
+        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+          validates :family_name_kana
+          validates :first_name_kana
+        end
+
+        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+        validates_format_of :password, with: PASSWORD_REGEX
+
 end
