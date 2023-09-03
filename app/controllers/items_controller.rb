12528d039c5ce3764d2310ea6631e69b5aeefcc1
ryomas501 <141004549+ryomas501@users.noreply.github.com>
2023-09-03 17:59:34 +0900

    itemsコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -30,8 +30,6 @@ def edit
 end
 
 def update
-  @item = Item.find(params[:id])
-
   if @item.update(item_params)
     redirect_to item_path(@item.id)
   else
