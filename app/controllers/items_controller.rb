9b4a68eea1637a4d3ff36aef077d1f4b9218eec2
ryomas501 <141004549+ryomas501@users.noreply.github.com>
2023-09-03 17:45:16 +0900

    Edit・Updateルーティング・コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,10 +21,24 @@ class ItemsController < ApplicationController
   end
 
 def show
+end
 
+def edit
+  unless user_signed_in? && current_user.id == @item.user.id
+    redirect_to root_path
+  end
+end
+
+def update
   @item = Item.find(params[:id])
 
+  if @item.update(item_params)
+    redirect_to item_path(@item.id)
+  else
+    render :edit, status: :unprocessable_entity
+  end
 end
+
 
   private
 
@@ -37,4 +52,8 @@ end
 
     redirect_to new_user_session_path
   end
+end
+
+def set_item
+  @item = Item.find(params[:id])
 end
