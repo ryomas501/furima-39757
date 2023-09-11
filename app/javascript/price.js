1157d10c8f0e9468b5f49233f75008d6eb2445ce
ryomas501 <141004549+ryomas501@users.noreply.github.com>
2023-09-11 14:17:40 +0900

    Java修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", function () {
       const inputValue = parseFloat(priceInput.value);
       const commission = document.getElementById("add-tax-price");
       commission.innerHTML = Math.floor(inputValue * 0.1);
+      commission.innerHTML = commissionValue;
       const profitNumber = document.getElementById("profit");
       const value_result = inputValue * 0.1;
       profitNumber.innerHTML = Math.floor(inputValue - value_result);
+      profitNumber.innerHTML = profitValue;
     });
   }
 });
