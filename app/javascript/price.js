51a1cc9f312473c0798c6002ac7385c0dc5aee85
ryomas501 <141004549+ryomas501@users.noreply.github.com>
2023-09-11 15:20:57 +0900

    Javascript再修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -2,14 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const priceInput = document.getElementById("item-price");
   if (priceInput) {
     priceInput.addEventListener("input", () => {
-      const inputValue = parseFloat(priceInput.value);
+      const inputValue = priceInput.value;
+      console.log("inputValue:", inputValue);
       const commission = document.getElementById("add-tax-price");
       commission.innerHTML = Math.floor(inputValue * 0.1);
-      commission.innerHTML = commissionValue;
+      console.log(commission);
+
       const profitNumber = document.getElementById("profit");
-      const value_result = inputValue * 0.1;
+      const value_result =  Math.floor(inputValue * 0.1);
       profitNumber.innerHTML = Math.floor(inputValue - value_result);
       profitNumber.innerHTML = profitValue;
+      console.log(profitNumber);
     });
   }
 });
